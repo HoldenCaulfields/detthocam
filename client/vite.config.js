08d5f3d25c5815e748f7fce6c294ceceb7c4d5c2
HoldenCaulfields <nguyenthianh1232345@gmail.com>
2025-02-23 09:23:43 +0700

    add base / in client vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Base path for the project
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist", // Output folder for Express to serve
