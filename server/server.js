0256d1f75b14a2ec4118698ca798937a4c29bf6d
HoldenCaulfields <nguyenthianh1232345@gmail.com>
2025-02-23 09:12:19 +0700

    change path name in server.js

--- server/server.js
+++ server/server.js
@@ -8,17 +8,12 @@ app.use(cors({ origin: "https://detthocamm.onrender.com"}));
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-/* app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-}); */
-
-app.get("/api/data", (req, res) => {
-    res.json({ message: "Hello from the backend!" });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
