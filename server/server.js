d709a08fd22ec65b141d448a711a2badfe20c04d
HoldenCaulfields <nguyenthianh1232345@gmail.com>
2025-02-23 11:43:04 +0700

    update server 1

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 console.log(path.join(__dirname, '../client/dist'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+    res.sendFile(path.join(process.cwd(),'client','dist','index.html'));
 });
 
 
